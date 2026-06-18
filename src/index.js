19a6ff3149760c4d34bf23b8a06fec92acce08b8
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-06-18 15:56:32 +0530

    cors-middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import mongoose  from "mongoose";
-
+import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path:'./env'
 })
+let  PORT=process.env.PORT || 8000
+connectDB()
+.then((res)=>{
+    app.on("Error",(error)=>{
+        console.log("Error:",error);
+    })
+    app.listen(PORT,()=>{
+        console.log(`Server is running at port ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED !!",err)
 
-connectDB();
+})
 /*import express from "express";
 
 const app=express();
