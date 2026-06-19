ed5760b3d14aa134e226009cb98cb50ce7d9214d
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-06-19 13:27:54 +0530

    ApiResponse AND ApiError

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,17 @@
-const asyncHandler=()=>{
-
-}
+const asyncHandler=(requestHandler)=>{(req,res,next)=>{
+    Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err));
+}}
 
 export default asyncHandler
 
-const asyncHandler=(fn)=>(req,res,next)=>{}
+// const asyncHandler=(fn)=>async (req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+
+//     }catch(error){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
