abd5a00ef261a537c6d7665e359543006b4d8f41
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-06-21 14:17:15 +0530

    Postman

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app=express();
-
+app.get('/',(req,res)=>{
+    res.send("Rupesh Kumar Chaudhary")
+})
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true,
@@ -19,7 +21,7 @@ import userRouter from "./routes/user.routes.js";
 
 //routes declaration
 
-app.use('api/v1/users',userRouter)
-//http:localhost:6000/api/v1/users/register/
+app.use('/api/v1/users',userRouter)
+//http:localhost:4000/api/v1/users/register/
 
 export default app;
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 const userRegister=asyncHandler(async (req,res)=>{
     res.status(200).json({
         message:"ok",
+        name:"RUPESH KUMAR CHAUDHARY ",
     })
 })
 
