2a162fc774ba4e2b4f92579b1e7c245086941649
Rupesh Kumar Chaudhary <rupesh.chaudhary.cs28@iilm.edu>
2026-06-24 22:40:37 +0530

    Backened

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,7 +4,18 @@ const userRegister=asyncHandler(async (req,res)=>{
     res.status(200).json({
         message:"ok",
         name:"RUPESH KUMAR CHAUDHARY ",
-        
+
+    //get user details from frontened
+    //validation-no field should be empty
+    //check if user already existed-from username ,password
+    //check for images,check for avatar
+    //upload images and avatar to cloudinary
+    //create user object and placed it into db
+    //remove password and refresh token from response
+    //check for user creation
+    //if user is created send response if not created
+    //send error
+
     })
 })
 
